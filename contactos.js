fe841fc6531d8026ec30155926a9e048ca216bcc
liz <lizethe1292@gmail.com>
2023-09-25 10:14:58 -0500

    he agregado un nuevo contacto y la función de actualizar contacto al archivo contactos.js

--- contactos.js
+++ contactos.js
@@ -26,6 +26,13 @@ crearContacto(3, "María", "Castañeda", "3150573457", [
   }
 ]);
 
+crearContacto(4, "Alejandro", "Carrillo", "3205674321", [
+  {
+    ciudad: "Manizales",
+    direccion: "Calle 88 #34-09"
+  }
+]);
+
 console.log("Agenda después de crear contactos:");
 console.log(agenda);
 
@@ -40,7 +47,29 @@ function eliminarContacto(id) {
   }
 }
 
-eliminarContacto(1);
+eliminarContacto(4);
 
 console.log("Agenda después de eliminar un contacto:");
+console.log(agenda);
+
+//Función para actualizar contacto
+function actualizarContacto(id, nuevosDatos) {
+  const contactoExistente = agenda.find((contacto) => contacto.id === id);
+
+  if (!contactoExistente) {
+    console.log(`No se encontró un contacto con ID ${id}.`);
+    return;
+  }
+
+  // Actualiza los campos del contacto existente con los nuevos datos
+  Object.assign(contactoExistente, nuevosDatos);
+  console.log(`Contacto con ID ${id} actualizado.`);
+}
+
+actualizarContacto(2, {
+  apellidos: "Escobar Sánchez",
+  telefono: "3137769045"
+});
+
+console.log("Agenda después de actualizar el contacto:");
 console.log(agenda);
